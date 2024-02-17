b596b9f99426ea91cf5f1392ea0f8187aa9bd571
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-02-18 00:15:30 +0600

    nav var added

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "primary-dark": "#030712",
+        "secondary-dark": "#03071299",
+        "primary-green": "#1DD100",
+      },
+    },
+  },
+};
